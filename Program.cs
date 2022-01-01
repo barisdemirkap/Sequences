9f25f82b88d503ad99e4192f17cb465ad24e6ee1
barisdemirkap <barisdemirkap@gmail.com>
2022-01-01 10:03:35 +0300

    initial commit of sequences

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System ;
+namespace _sequences 
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            //The definition of the sequences
+            string[] colors = new string[5] ;
+
+            string[] animals = {"kedi","kopek","kus","maymun","bocek"} ;
+            int[] _seq ;
+            _seq = new int[5]  ;
+            // value allocation to a sequence
+            colors[0] = "Blue" ;
+            _seq[3] = 10;
+            Console.WriteLine(animals[1]);
+            Console.WriteLine(colors[0]);
+            Console.WriteLine(_seq[3]);
+
+
+
+        }
+          }
+}
